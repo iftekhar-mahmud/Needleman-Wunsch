ccba5b4ac9303407ac833908afcb46ae3b540793
Iftekhar Mahmud <74108817+iftekhar-mahmud@users.noreply.github.com>
2023-08-21 00:02:12 +0600

    Update Needleman.cpp

--- Needleman.cpp
+++ Needleman.cpp
@@ -13,13 +13,14 @@ int gap_penalty = -2;
 int max3(int a, int b, int c) {
     return max(a, max(b, c));
 }
-
+//max3 is a helper function that returns the maximum of three integers a, b, and c using the max function from the <algorithm> library.
 void needleman_wunsch(const string& sequence1, const string& sequence2) {
     int rows = sequence1.length() + 1;
     int cols = sequence2.length() + 1;
 
     vector<vector<int>> matrix(rows, vector<int>(cols, 0));
-
+//needleman_wunsch is the main function implementing the Needleman-Wunsch algorithm. It takes two input sequences as strings. The function initializes the number of rows and columns for the matrix based on the lengths of the sequences.
+    //A 2D vector named matrix is created with dimensions (rows, cols) and initialized with zeros. This matrix will store the scores for each alignment position.
     // Initialize the first row and column
     for (int i = 1; i < rows; ++i) {
         matrix[i][0] = matrix[i - 1][0] + gap_penalty;
@@ -27,7 +28,8 @@ void needleman_wunsch(const string& sequence1, const string& sequence2) {
     for (int j = 1; j < cols; ++j) {
         matrix[0][j] = matrix[0][j - 1] + gap_penalty;
     }
-
+   //These loops initialize the first row and first column of the matrix using gap penalties. The scores are calculated based on the scores from adjacent cells in the matrix.
+    
     // Fill in the matrix
     for (int i = 1; i < rows; ++i) {
         for (int j = 1; j < cols; ++j) {
@@ -37,7 +39,12 @@ void needleman_wunsch(const string& sequence1, const string& sequence2) {
             matrix[i][j] = max3(match, deleteOp, insertOp);
         }
     }
-
+  //These nested loops fill in the rest of the matrix using the Needleman-Wunsch dynamic programming approach. For each cell (i, j), three possible operations are considered: match/mismatch, delete (gap in sequence1), and insert (gap in sequence2).
+  //The match score is calculated based on the previous diagonal cell score, either adding match_score for a match or mismatch_score for a mismatch.
+  //The deleteOp score is based on the score above the current cell, adding the gap penalty.
+  //The insertOp score is based on the score left of the current cell, adding the gap penalty.
+  //The maximum of these three scores is taken using the max3 function and stored in the current cell (i, j).
+    
     // Backtrack to find the optimal alignment
     string alignment1 = "";
     string alignment2 = "";
@@ -60,6 +67,9 @@ void needleman_wunsch(const string& sequence1, const string& sequence2) {
         }
     }
 
+//This loop performs the traceback step to reconstruct the optimal alignment by backtracking through the matrix from the bottom-right corner (rows-1, cols-1) to the top-left corner (0, 0).
+//Depending on the maximum score in the current cell, the algorithm determines whether the optimal path involved a match/mismatch, a gap in sequence1, or a gap in sequence2. The corresponding characters are added to alignment1 and alignment2.
+    
     while (i > 0) {
         alignment1 = sequence1[i - 1] + alignment1;
         alignment2 = "-" + alignment2;
@@ -74,7 +84,8 @@ void needleman_wunsch(const string& sequence1, const string& sequence2) {
     cout << "Alignment 1: " << alignment1 << endl;
     cout << "Alignment 2: " << alignment2 << endl;
 }
-
+//After the traceback, any remaining characters in either sequence are added to the alignments, representing gaps.
+//The final alignments are printed to the console.
 int main() {
     string sequence1 = "AGTACGCA";
     string sequence2 = "TATGC";
@@ -82,4 +93,5 @@ int main() {
 
     return 0;
 }
-
+//The main function initializes the input sequences and calls the needleman_wunsch function with them.
+//It returns 0 to indicate successful execution.
